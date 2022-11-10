b5755cea39d813ff0347a31ecdfdc6a6735e4ba5
Okoro Godwin Chinedu <64032658+Aysha-py@users.noreply.github.com>
2022-11-10 14:06:16 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import '../src/Styles/App.css';
+import { useEffect, useState } from "react";
+import { ListNft } from "./components/ListNft";
+import { RetrieveNft } from "./components/RetrieveNft";
+import bunzz from "bunzz-sdk";
 
-function App() {
+const DAPP_ID = process.env.REACT_APP_DAPP_ID;
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const App = () => {
+  const [handler, setHandler] = useState();
+  const [userAddress, setUserAddress] = useState("");
+
+  useEffect(() => {
+    setup();
+  }, []);
+
+  const setup = async () => {
+    const handler = await bunzz.initializeHandler({
+      dappId: DAPP_ID,
+      apiKey: API_KEY,
+    });
+
+    const userAddress = await handler.getSignerAddress();
+
+    console.log(userAddress);
+    setUserAddress(userAddress);
+    setHandler(handler);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center">
+      <div>
+        <ListNft bunzz={handler} userAddress={userAddress} />
+        <RetrieveNft bunzz={handler} userAddress={userAddress} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;

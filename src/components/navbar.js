b63454093d6bbea111b3d8b5fa941eca96d88152
Manyo-Rocks <judithmanyo2@gmail.com>
2022-11-10 17:12:16 +0100

    updating

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+// import {appLogo} from "../image/appLogo"
+
+
+
+function NavBar (){
+    return(
+        <>
+            <nav>
+                <div classname="logo">
+                    {/* <img src={appLogo} alt="applogo" ></img> */}
+                </div>
+            </nav>
+        </>
+    )
+}
+export {NavBar}

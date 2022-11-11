337b6f653ccb8e5d01002cb82280602663e08224
Manyo-Rocks <judithmanyo2@gmail.com>
2022-11-11 17:09:40 +0100

    updating

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export  const data=[
+    {
+         id:1,
+         question: "Submit bitcoin whiteboard image",
+    },
+    {
+        id:2,
+        question: "Submit bitcoin whiteboard image",
+    },  
+    {
+        id:3,
+        question: "Submit bitcoin whiteboard image",
+    },
+    {
+        id:4,
+        question: "Submit bitcoin whiteboard image",
+    },
+    {
+        id:5,
+        question: "Submit bitcoin whiteboard image",
+    },
+    {
+        id:6,
+        question: "Submit bitcoin whiteboard image",
+    },
+ ]
